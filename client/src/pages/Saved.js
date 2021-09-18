a65f96fca4ebdcc233fcfb8b2ced959905633266
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-18 16:21:03 -0400

    Modifications: added pages folder; included Search, Saved, and NoMatch pages; installed react-router-dom; updated App.js to use Router to navigate to Search and Saved pages.

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Saved() {
+
+
+  return (
+    <div>
+        Test
+    </div>
+  );
+}
+
+export default Saved;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import API from "../utils/API";
+import { BookList, BookListItem } from "../components/BookList";
+import { Container, Row, Col } from "../components/Grid";
+
+function Search() {
+
+  const [books, setBooks] = useState([]);
+  const [bookSearch, setBookSearch] = useState("");
+
+  const handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { value } = event.target;
+    setBookSearch(value);
+  };
+
+  const handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get books update the books state
+    event.preventDefault();
+    API.getBooks(bookSearch)
+      .then( 
+        res =>{
+          console.log(res.data.items);
+          setBooks(res.data.items)
+        }) 
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <form>
+              <Container>
+                <Row>
+                  <Col size="xs-9 sm-10">
+                    <Input
+                      name="BookSearch"
+                      value={bookSearch}
+                      onChange={handleInputChange}
+                      placeholder="Search For a Book"
+                    />
+                  </Col>
+                  <Col size="xs-3 sm-2">
+                    <Button
+                      onClick={handleFormSubmit}
+                      type="success"
+                      className="input-lg"
+                    >
+                        Search
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="xs-12">
+            {!books.length ? (
+              <h1 className="text-center">No Books to Display</h1>
+            ) : (
+              <BookList>
+                {books.map(book => {
+                  return (
+                    <BookListItem
+                      key={book.id}
+                      title={book.volumeInfo.title}
+                      description={book.volumeInfo.description}
+                      link={book.volumeInfo.infoLink}
+                      authors={book.volumeInfo.authors}
+                      thumbnail={book.volumeInfo.imageLinks.thumbnail}
+                    />
+                  );
+                })}
+              </BookList>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Search;
