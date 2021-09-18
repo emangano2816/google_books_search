f7123ba14484978afe83ccc73c69eee98105b8ca
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-18 12:02:18 -0400

    Updated with Google Book Search references.

--- client/src/Components/Jumbotron/index.js
+++ client/src/Components/Jumbotron/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 function Jumbotron() {
   return (
     <div className="jumbotron text-center">
-      <h1>React Recipes</h1>
-      <a target="_blank" rel="noopener noreferrer" href="http://www.recipepuppy.com/about/api/">
-        Powered by Recipe Puppy
+      <h1>(React) Google Books Search</h1>
+      <a target="_blank" rel="noopener noreferrer" href="https://developers.google.com/books/">
+        Search for and Save Books of Interest
       </a>
     </div>
   );
