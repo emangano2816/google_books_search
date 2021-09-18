410e880182de15fdbb5952e008309aabbdd0ca63
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-18 14:37:20 -0400

    Removed un-needed files from create-react-app; copied App.js from 21-MERN Act02; changed recipe references to book.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,90 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import API from "./utils/API";
+import { BookList, BookListItem } from "./components/BookList";
+import { Container, Row, Col } from "./components/Grid";
 
 function App() {
+
+  const [books, setBooks] = useState([]);
+  const [bookSearch, setBookSearch] = useState("");
+
+  const handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { value } = event.target;
+    setBookSearch(value);
+  };
+
+  const handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get books update the books state
+    event.preventDefault();
+    API.getBooks(bookSearch)
+      .then(res => setBooks(res.data))
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div>
+      <Nav />
+      <Jumbotron />
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <form>
+              <Container>
+                <Row>
+                  <Col size="xs-9 sm-10">
+                    <Input
+                      name="BookSearch"
+                      value={bookSearch}
+                      onChange={handleInputChange}
+                      placeholder="Search For a Book"
+                    />
+                  </Col>
+                  <Col size="xs-3 sm-2">
+                    <Button
+                      onClick={handleFormSubmit}
+                      type="success"
+                      className="input-lg"
+                    >
+                        Search
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="xs-12">
+            {!books.length ? (
+              <h1 className="text-center">No Books to Display</h1>
+            ) : (
+              <BookList>
+                {books.map(book => {
+                  return (
+                    <BookListItem
+                      key={book.title}
+                      title={book.title}
+                      description={book.description}
+                      href={book.link}
+                      authors={book.authors}
+                      thumbnail={book.thumbnail}
+                    />
+                  );
+                })}
+              </BookList>
+            )}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
-
 export default App;
+
