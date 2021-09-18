60491188b6e1003f34dc443273ed22156b018c6f
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-09-18 11:41:56 -0400

    Removed options from mongoose.connect that are not supported any longer.

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
     }
 );
 
